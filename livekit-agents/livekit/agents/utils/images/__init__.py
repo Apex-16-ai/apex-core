6c367ec757045910708338e73a7f289548097363
Apex-16-ai <aminefriraml@gmail.com>
2024-09-14 09:46:12 -0700

    explored service mesh concepts, network calls direct; theoretical request routing.

--- livekit-agents/livekit/agents/utils/images/__init__.py
+++ livekit-agents/livekit/agents/utils/images/__init__.py
@@ -15,3 +15,5 @@
 from .image import EncodeOptions, ResizeOptions, encode
 
 __all__ = ["EncodeOptions", "ResizeOptions", "encode"]
+# Modified on Tue Jan 21 11:33:44 PM +01 2025
+print('Debug message')
