cde0e710d9aafd70060f951753afc214cb6a9fd7
Apex-16-ai <aminefriraml@gmail.com>
2024-09-19 13:53:05 -0700

    pondered orm abstractions, sql queries unchanged; data access theorizing.

--- livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/__init__.py
+++ livekit-plugins/livekit-plugins-playai/livekit/plugins/playai/__init__.py
@@ -30,3 +30,5 @@ print('Debug message')
 print('Debug message')
 # Modified on Tue Jan 21 11:33:42 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:33:45 PM +01 2025
+print('Debug message')
--- livekit-plugins/livekit-plugins-rime/livekit/plugins/rime/models.py
+++ livekit-plugins/livekit-plugins-rime/livekit/plugins/rime/models.py
@@ -6,3 +6,5 @@ TTSEncoding = Literal[
     "pcm",
     "mp3",
 ]
+# Modified on Tue Jan 21 11:33:45 PM +01 2025
+print('Debug message')
