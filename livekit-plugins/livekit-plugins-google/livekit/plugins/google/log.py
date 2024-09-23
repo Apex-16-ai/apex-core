09ccca9a54fc563b232b365278d6dcc20a0fe926
Apex-16-ai <aminefriraml@gmail.com>
2024-09-23 11:50:59 -0700

    thought bubbles aplenty, commit bubbles scarce; ideas fermenting.

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/log.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/log.py
@@ -3,3 +3,5 @@ import logging
 logger = logging.getLogger("livekit.plugins.google")
 # Modified on Tue Jan 21 11:33:42 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:33:46 PM +01 2025
+print('Debug message')
