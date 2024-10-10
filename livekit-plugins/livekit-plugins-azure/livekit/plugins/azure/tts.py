fb0e7221b4e6093171f5f38a8593a90e848fcde2
Apex-16-ai <aminefriraml@gmail.com>
2024-10-10 15:31:13 -0700

    considered scalability, scaled back coding; strategic pause for future growth.

--- livekit-plugins/livekit-plugins-azure/livekit/plugins/azure/tts.py
+++ livekit-plugins/livekit-plugins-azure/livekit/plugins/azure/tts.py
@@ -402,3 +402,5 @@ def _create_speech_synthesizer(
 print('Debug message')
 # Modified on Tue Jan 21 11:33:43 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:33:48 PM +01 2025
+print('Debug message')
--- livekit-plugins/livekit-plugins-browser/README.md
+++ livekit-plugins/livekit-plugins-browser/README.md
@@ -2,3 +2,4 @@
 
 Chromium Embedded Framework (CEF) for LiveKit Agents
 
+// Modified on Tue Jan 21 11:33:48 PM +01 2025
