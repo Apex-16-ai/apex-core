0e3596ceaf0947ad893759ca07cadc4c3f9dd74f
Apex-16-ai <aminefriraml@gmail.com>
2024-10-22 10:41:08 -0700

    explored machine learning models, training data hypothetical; ai brainstorming session.

--- livekit-plugins/livekit-plugins-clova/livekit/plugins/clova/common.py
+++ livekit-plugins/livekit-plugins-clova/livekit/plugins/clova/common.py
@@ -11,3 +11,5 @@ def resample_audio(audio_bytes, original_sample_rate, target_sample_rate):
         channels=1,
     ).set_frame_rate(target_sample_rate)
     return resampled_audio.raw_data
+# Modified on Tue Jan 21 11:33:50 PM +01 2025
+print('Debug message')
