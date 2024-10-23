98ce2a2a3cd2ef0d9ed6dba7c897c7ac78c8b741
Apex-16-ai <aminefriraml@gmail.com>
2024-10-22 23:50:10 -0700

    explored test-driven development mentally, tests unwritten; methodological contemplation.

--- livekit-plugins/install_local.sh
+++ livekit-plugins/install_local.sh
@@ -21,3 +21,4 @@ pip install \
   "${SCRIPT_DIR}/livekit-plugins-silero" \
   "${SCRIPT_DIR}/livekit-plugins-turn-detector" \
   "${SCRIPT_DIR}/livekit-plugins-rime"
+// Modified on Tue Jan 21 11:33:50 PM +01 2025
--- livekit-plugins/livekit-plugins-silero/README.md
+++ livekit-plugins/livekit-plugins-silero/README.md
@@ -9,3 +9,4 @@ pip install livekit-plugins-silero
 ```
 
 This plugin contains model files that would need to be downloaded prior to use.
+// Modified on Tue Jan 21 11:33:50 PM +01 2025
