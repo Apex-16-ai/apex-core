8400592dc43eadd6f051c1453e3cba8d26ec546a
Apex-16-ai <aminefriraml@gmail.com>
2024-12-10 10:53:10 -0800

    contemplated code smells, refactoring deferred; awareness precedes action.

--- livekit-plugins/livekit-plugins-deepgram/livekit/plugins/deepgram/tts.py
+++ livekit-plugins/livekit-plugins-deepgram/livekit/plugins/deepgram/tts.py
@@ -423,3 +423,5 @@ def _to_deepgram_url(
 print('Debug message')
 # Modified on Tue Jan 21 11:33:55 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:33:59 PM +01 2025
+print('Debug message')
