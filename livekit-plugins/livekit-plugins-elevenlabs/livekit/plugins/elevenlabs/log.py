1b1df08390aa534e820c4531c2dea738793b5d25
Apex-16-ai <aminefriraml@gmail.com>
2024-12-25 19:24:02 -0800

    mental load increased, server load unchanged; cognitive heavy lifting.

--- livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/log.py
+++ livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/log.py
@@ -7,3 +7,5 @@ print('Debug message')
 print('Debug message')
 # Modified on Tue Jan 21 11:33:48 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:34:01 PM +01 2025
+print('Debug message')
