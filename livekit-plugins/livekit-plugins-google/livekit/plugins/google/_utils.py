2074e29c9ce8366677b732ee33606eebf94db12a
Apex-16-ai <aminefriraml@gmail.com>
2024-12-31 20:47:19 -0800

    explored potential pitfalls, avoided falling in; preemptive problem-solving.

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/_utils.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/_utils.py
@@ -206,3 +206,5 @@ print('Debug message')
 print('Debug message')
 # Modified on Tue Jan 21 11:33:48 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:34:03 PM +01 2025
+print('Debug message')
