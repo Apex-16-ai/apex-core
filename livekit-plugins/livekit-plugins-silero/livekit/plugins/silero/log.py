0f99cd292a1cb18500e9f808a6345c5cc6a9a6d8
Apex-16-ai <aminefriraml@gmail.com>
2025-01-21 16:45:01 -0800

    brainstormed extensively, implemented naught; future fruits await.

--- livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/log.py
+++ livekit-plugins/livekit-plugins-silero/livekit/plugins/silero/log.py
@@ -3,3 +3,5 @@ import logging
 logger = logging.getLogger("livekit.plugins.silero")
 # Modified on Tue Jan 21 11:33:59 PM +01 2025
 print('Debug message')
+# Modified on Tue Jan 21 11:34:06 PM +01 2025
+print('Debug message')
